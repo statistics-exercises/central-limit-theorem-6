10196fe2f1660f57b1f82ef9102ede1889e93ba4
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-17 12:06:39 +0100

    Added answers

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ def mean_with_errors(n) :
   # Your code to calculate the sample mean and sample variance 
   # for a set of n uniform random variables between 0 and 1 goes 
   # here.  
-  
+  S, S2 = 0,0 
+  for i in range(n) : 
+      myvar = np.random.uniform(0,1)
+      S = S + myvar 
+      S2 = S2 + myvar*myvar
+  mean = S / n
+  var = (n/(n-1))*( S2/n - mean*mean )
+  lower = mean + np.sqrt(var/n)*scipy.stats.norm.ppf(0.05)
+  upper = mean + np.sqrt(var/n)*scipy.stats.norm.ppf(0.95) 
   
   # When complete this function should return
   # lower = the 5th percentile of the distribution that was sampled
