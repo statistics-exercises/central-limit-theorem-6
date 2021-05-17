1a605fa3f6a7c45dfb5cc418be5db54e9a8b7029
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-17 11:35:50 +0100

    Setup tests

---
+++ testing/test_main.py
@@ -0,0 +1,50 @@
+try:
+    from AutoFeedback.funcchecks import check_func 
+except:
+    import subprocess
+    import sys
+            
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
+    from AutoFeedback.funcchecks import check_func 
+           
+from AutoFeedback.randomclass import randomvar
+import unittest
+from main import *
+
+def first_arg(n) :
+   low, mean, up = mean_with_errors(n)
+   return (( low - mean) / scipy.stats.norm.ppf(0.05) )**2   
+
+def second_arg(n) :
+   low, mean, up = mean_with_errors(n)
+   return mean 
+
+def third_arg(n) :
+   low, mean, up = mean_with_errors(n)
+   return (( up - mean) / scipy.stats.norm.ppf(0.95) )**2 
+
+class UnitTests(unittest.TestCase) :
+   def test_first_arg(self) : 
+       inputs, var  = [], []
+       for i in range(2,10) :
+           inputs.append((i,))
+           myvar1 = randomvar( 0.5, variance=1/12/i, dist="chi2", isinteger=False )
+           var.append(myvar1)
+       assert( check_func("first_arg", inputs, var) )
+
+   def test_second_arg(self) : 
+       inputs, var  = [], []
+       for i in range(2,10) :
+           inputs.append((i,))
+           myvar1 = randomvar( 0.5, variance=1/12/i, vmin=0, vmax=1, isinteger=False )
+           var.append(myvar1)
+       assert( check_func("second_arg", inputs, var) )
+
+   def test_third_arg(self) : 
+       inputs, var  = [], []
+       for i in range(2,10) :
+           inputs.append((i,))
+           myvar1 = randomvar( 0.5, variance=1/12/i, dist="chi2", isinteger=False )
+           var.append(myvar1)
+       assert( check_func("third_arg", inputs, var) )
+     
